82fc592c1873de690b8b1962b1b0d5cfb8346fcc
Jon-Michael Narvaez <jon-michaelnarvaez@jms-macbook-pro.attlocal.net>
2020-08-25 14:49:50 -0500

    updates to package.json

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,11 @@ import React from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Button from "react-bootstrap/Button";
 import "./Home.css";
-import Image from 'react-bootstrap/Image';
 
 const Home = (props) => {
   return (
     <Jumbotron
-      className="text-center d-none d-sm-block d-md-block"
-   >
-   <Image src="/public/images/AdobeStock_214628027.jpeg"/>
+      className="text-center d-none d-sm-block d-md-block">
       <h1>Let's, talk.</h1>
       <p>
         A safe place for athlete from all backgrounds to reset, reacharge and
