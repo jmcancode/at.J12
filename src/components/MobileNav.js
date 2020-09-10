b9de886a2e1e67de41cd1e83992e1fbb2eb61670
Jon-Michael Narvaez <jon-michaelnarvaez@jms-macbook-pro.attlocal.net>
2020-09-10 18:23:23 -0500

    updates to mobile na

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,17 +1,14 @@
 import { render } from 'node-sass';
 import React from 'react';
+import Sidebar from 'react-sidebar';
 
 import Navbar from 'react-bootstrap/Navbar';
 
 
 class mobileNav extends React.Component {
-    constructor(props);
+constructor(props) {
+    super(props);
     this.state = {
-        count: 0
-    };
-}
-render() {
-    return(
-        
-    )
+        sidebarOpen: true
+    }
 }
