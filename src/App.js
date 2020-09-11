f38c57c0b31776e65fecbebf3faaef6b41ed0e34
Jon-Michael Narvaez <jon-michaelnarvaez@jms-macbook-pro.attlocal.net>
2020-09-11 15:02:38 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -11,57 +11,52 @@ import Settings from "./pages/Settings";
 import EditProfile from "./pages/EditProfile";
 import Messages from "./pages/Messages";
 import Login from "./pages/Login";
-import ToolBar from "./components/ToolBar.js/ToolBar";
+import ToolBar from "./components/ToolBar/ToolBar";
 import SideDrawer from "./components/SideDrawer/SideDrawer";
 import BackDrop from "./components/SideDrawer/BackDrop/BackDrop";
 
 class App extends Component {
   state = {
-    sideDrawerOpen: false
+    sideDrawerOpen: false,
   };
 
-  drawerToggleClickHandler =() => {
+  drawerToggleClickHandler = () => {
     this.setState((prevState) => {
-      return{sideDrawerOpen : !prevState.sideDrawerOpen};
+      return { sideDrawerOpen: !prevState.sideDrawerOpen };
     });
   };
 
   backdropClickHandler = () => {
-    this.setState({sideDrawerOpen: false})
-
+    this.setState({ sideDrawerOpen: false });
   };
 
-
-
-
   render() {
-    
     let backdrop;
 
-    if (this.state.sideDrawerOpen){
-      backdrop = <BackDrop click={this.drawerToggleClickHandler}/>;
+    if (this.state.sideDrawerOpen) {
+      backdrop = <BackDrop click={this.drawerToggleClickHandler} />;
     }
-  return (
-    <div style={{ height: '100%'}}>
-      <BrowserRouter>
-        <Navigation />
-        <ToolBar drawerClickHandler={this.drawerToggleClickHandler}/>
-        <SideDrawer show={this.state.sideDrawerOpen}/>
-        {backdrop}
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/home" component={Home} />
-          <Route path="/addpost" component={AddPost} />
-          <Route path="/profile" component={Profile} />
-          <Route path="/plans" component={Plans} />
-          <Route path="/about" component={About} />
-          <Route path="/settings" component={Settings} />
-          <Route path="/edit-profile" component={EditProfile} />
-          <Route path="/message" component={Messages} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+      <div style={{ height: "100%" }}>
+        <BrowserRouter>
+          <Navigation />
+          <ToolBar drawerClickHandler={this.drawerToggleClickHandler} />
+          <SideDrawer show={this.state.sideDrawerOpen} />
+          {backdrop}
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/home" component={Home} />
+            <Route path="/addpost" component={AddPost} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/plans" component={Plans} />
+            <Route path="/about" component={About} />
+            <Route path="/settings" component={Settings} />
+            <Route path="/edit-profile" component={EditProfile} />
+            <Route path="/message" component={Messages} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
