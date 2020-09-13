f0b7716ee27d0297c54a6367a78d25a9fea0e519
unknown <carlos.delgado.dev@gmail.com>
2020-09-12 22:55:41 -0500

    Started work on Plans page

---
+++ src/components/Plans/PlanHeader.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './PlanHeader.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSlidersH, faFileAlt } from '@fortawesome/free-solid-svg-icons';
+
+const PlanHeader = () => {
+  return (
+    <div className="d-flex align-content-center justify-content-between header-container mx-2">
+      <div className="d-flex align-items-center mr-4">
+        <div className="mr-2">
+          <FontAwesomeIcon icon={faSlidersH} color="#BF5700" />
+        </div>
+        <p className="mb-0">Filter</p>
+      </div>
+
+      <h3 className="mb-0">PLANS</h3>
+
+      <div className="d-flex align-items-center">
+        <div className="mr-2">
+          <FontAwesomeIcon icon={faFileAlt} color="#BF5700" />
+        </div>
+        <p className="mb-0">My Plans</p>
+      </div>
+    </div>
+  );
+};
+
+export default PlanHeader;
