a6066b89a64e4e8154b1ca95172e28a55766a0bb
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-09-14 18:58:34 -0500

    changed some padding

--- src/pages/Plans.js
+++ src/pages/Plans.js
@@ -5,7 +5,7 @@ import PlanHeader from '../components/Plans/PlanHeader';
 
 const Plans = (props) => {
   return (
-    <div className="shadow rounded mx-3 mb-3 p-3">
+    <div className="shadow rounded mx-3 mt-2 mb-3 pr-3 pb-3 pl-3">
       <PlanHeader />
       <PlanCategory />
       <PlanCategory />
