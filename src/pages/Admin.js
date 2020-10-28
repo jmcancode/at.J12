e47bd1869526c3076b4788152f0359b18fb2afa2
Jon-Michael Narvaez <you@example.com>
2020-10-28 08:09:21 -0500

    cleaned up files

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -42,21 +42,27 @@ class Admin extends Component {
             </h3>
           </CardHeader>
           <CardBody>
-            <FormGroup>
+           
+          <FormGroup>
+          <Label>Plan Category</Label>
+          <Input type="select" name="select" id="exampleSelect">
+            <option>Social Justice</option>
+            <option>Personal Development</option>
+            <option>Pro Sports</option>
+            <option>Mental Health</option>
+            <option>Wellness</option>
+            <option>Life Skills</option>
+            <option>Ethnicity</option>
+            <option>Gender</option>
+          </Input>
+        </FormGroup>
+              <FormGroup>
+              <Label>Content Type</Label>
               <Input type="select" name="select" id="exampleSelect">
                 <option>Single Day</option>
                 <option>Multi-Day</option>
               </Input>
             </FormGroup>
-            <Form>
-              <Label>Plan Category</Label>
-              <div class="input-group-prepend"></div>
-              <input
-                type="text"
-                className="form-control mb-2"
-                aria-label="Small"
-                aria-describedby="inputGroup-sizing-sm"
-              />
               <Label>Plan Title</Label>
               <input
                 type="text"
@@ -64,7 +70,7 @@ class Admin extends Component {
                 aria-label="Small"
                 aria-describedby="inputGroup-sizing-sm"
               />
-            </Form>
+
             <Form>
               <Label>Plan Text</Label>
               <div
@@ -96,6 +102,15 @@ class Admin extends Component {
                 <input type="file" className="form-control-file" />
               </div>
             </Form>
+            <Button
+              color="primary"
+              size="sm"
+              className="mt-2"
+              block
+              style={{ backgroundColor: "#b57000", borderColor: "transparent" }}
+            >
+              Add Plan
+            </Button>
             <Button
               color="primary"
               size="sm"
